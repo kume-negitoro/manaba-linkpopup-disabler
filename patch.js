e275eeae36e37a50dcfb2dad7971106e9f7878fd
kume-negitoro <kume.negitoro@gmail.com>
2020-07-08 22:35:36 +0900

    change link target to _blank

--- patch.js
+++ patch.js
@@ -3,6 +3,7 @@ Array.from(
     document.querySelectorAll('a[href*="link_iframe_balloon?url="]'),
     el => {
         el.onclick = void 0
+        el.target = '_blank'
         const re = /link_iframe_balloon\?url=(.+)/
         el.href = decodeURIComponent(el.href.match(re)[1])
     }
